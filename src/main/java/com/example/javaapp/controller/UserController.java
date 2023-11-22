a6fc9af903648a4d707130f5802e6bb757739139
alex <907785012@qq.com>
2023-11-22 16:20:48 +0800

    Delete database operation

--- src/main/java/com/example/javaapp/controller/UserController.java
+++ src/main/java/com/example/javaapp/controller/UserController.java
@@ -1,21 +1,14 @@
 package com.example.javaapp.controller;
 
-import javax.annotation.Resource;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.javaapp.service.UserService;
-
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @Resource
-    private UserService userService;
-
     @GetMapping
     public String getUser() {
-        return userService.getUserList().toString();
+        return "Users";
     }
 }
--- src/main/java/com/example/javaapp/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.javaapp.service;
-
-import java.util.List;
-
-import com.example.javaapp.entity.User;
-
-public interface UserService {
-    List<User> getUserList();
-}
