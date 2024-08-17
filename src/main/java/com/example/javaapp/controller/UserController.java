cb8973cd5410d6f5727e52884a803574c6d7602a
Yolo <183725504@qq.com>
2024-08-17 15:25:20 +0800

    Update UserController.java four

--- src/main/java/com/example/javaapp/controller/UserController.java
+++ src/main/java/com/example/javaapp/controller/UserController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping
     public String getUser() {
-        return "Users Hello ,change , two,three";
+        return "Users Hello ,change , two,three,four";
     }
 }
